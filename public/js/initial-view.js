7f1fe04914decd82523dfbccc6a58dc443808325
Johan <johan-avila@outlook.com>
2021-02-10 01:54:51 -0600

    Remove button

--- public/js/initial-view.js
+++ public/js/initial-view.js
@@ -4,32 +4,42 @@ let userId = document.getElementById("userId").innerHTML;
 // let profileName = document.getElementById("profileName");
 
 //Redirect => media player
-let playButton =(video_url)=>{
-    if(video_url){
-        window.location.href= `/media-player/${btoa(video_url)}`
-    }else {
+let playButton = (video_url) => {
+    if (video_url) {
+        window.location.href = `/media-player/${btoa(video_url)}`
+    } else {
         console.log("No hay video")
     }
 }
-let addButton =async (movieId)=>{
-    console.log(movieId, userId);
-    let getMovie = await axios(`/movie/${movieId}`);
-    let movie = getMovie.data.data;
-    let createUserMovie = await axios(`/user-movies`,{
-        method:"post",
-        data:{
-            "userId": userId,
-            "movieId": movie
-        }
-    })
+let addButton = async (movieId) => {
+    try {
+        let getMovie = await axios(`/movie/${movieId}`);
+        let movie = getMovie.data.data;
+        let createUserMovie = await axios(`/user-movies`, {
+            method: "post",
+            data: {
+                "userId": userId,
+                "movieId": movie
+            }
+        })
+        
+        location.reload()
+    } catch (error) {
+
+    }
 
 }
 
-let removeButton = async () => {
-    console.log("removeButton");
+let removeButton = async (userMovieId) => {
+    let userMovieDeleted = await axios(`/user-movies/${userMovieId}`, {
+        method: "delete"
+    })
+    console.log(userMovieDeleted);
+    location.reload()
+
 }
 //Logout (this function delete token)
-logoutButton.onclick = ()=>{
+logoutButton.onclick = () => {
     document.cookie = "token= ;"
     window.location.href = "/login"
 }
